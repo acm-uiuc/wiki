5b4847456cb5aab5e3e042b029c78d5c4c11b948
Yanni Zhuang <yanniz3@illinois.edu>
2026-01-22 16:15:43 -0600

    gh action wants build not dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './build',
   integrations: [mdx()],
-
   vite: {
     plugins: [tailwindcss()]
   }
