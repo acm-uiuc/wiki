44393a83b1c3ccb9aa8d21b153d6a6580e35ce05
Dev Singh <dev@devksingh.com>
2026-02-15 14:22:21 -0600

    chore: SEO and content size optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://illinoiscs.wiki",
   outDir: './build',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap(), compress()],
   vite: {
     plugins: [tailwindcss()]
   }
