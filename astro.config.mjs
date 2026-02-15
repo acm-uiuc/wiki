353c6e9f2858d8078d35ef752ec51c80726d648c
Yanni Zhuang <35969859+zhuangy47@users.noreply.github.com>
2026-02-15 14:34:30 -0600

    [Technical Improvement] Last Modified Date (#15)
    * init last modified
    
    * modified time plugin
    
    * move last modified to slugs
    
    * eh?
    
    * remove extends, new line
    
    * im dumb
    
    * remove dayjs from baselayout
    
    * bleh
    
    * new line
    
    * I'm still stupid
    
    * new line

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { remarkModifiedTime } from './remark-modified-time.mjs';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
@@ -7,6 +8,9 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkModifiedTime]
+  },
   site: "https://illinoiscs.wiki",
   outDir: './build',
   integrations: [mdx(), sitemap(), compress()],
